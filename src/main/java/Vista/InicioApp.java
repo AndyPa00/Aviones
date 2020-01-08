6f33fde7030cf9a2b3176ce8d829f1270d3c30c4
AndyPa00 <andres.parra1617@gmail.com>
2020-01-08 13:05:34 +0100

    No puedo avanzar.
    Laura te toca hacer que cuando le des a registrarse o a iniciar sesión
    se abra un anchorpane es decir una ventana nueva.

--- src/main/java/Vista/InicioApp.java
+++ src/main/java/Vista/InicioApp.java
@@ -1,9 +1,18 @@
 package Vista;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.hibernate.Session;
 
 import Controlador.ControladorRaiz;
+import Modelo.Credencial;
+import Utilidad.Utilidades2;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -12,6 +21,7 @@ import javafx.stage.Stage;
 public class InicioApp extends Application{
 	private Stage escena;
 	private BorderPane raiz;
+	private Session session1;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -41,12 +51,20 @@ public class InicioApp extends Application{
 		escena.show();
 	}
 	
+	@FXML
 	public void registrarse() {
-		
+		session1 = Utilidades2.getSessionFactory().openSession();
+		List<Credencial> listaUsuarios = new ArrayList<Credencial>();
+		TypedQuery<Credencial> query = session1.createQuery("FROM Credencial", Credencial.class);
+		listaUsuarios = query.getResultList();
+		for (int i = 0; i < listaUsuarios.size(); i++) {
+			System.out.println(listaUsuarios.get(i).toString());
+		}
 	}
 	
 	public void iniciarSesion() {
-		
+
+		Utilidades2.getSessionFactory().close();
 	}
 	
 	public void recuperarUsuario() {
@@ -56,4 +74,5 @@ public class InicioApp extends Application{
 	public void recuperarContrasena() {
 		
 	}
+
 }
